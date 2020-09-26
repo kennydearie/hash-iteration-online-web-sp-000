e89d8bd2741ff618d423acdc27a4d1268578934a
Nkenge Simms <simmsn@yahoo.com>
2020-09-26 02:08:25 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids <= 12
+  if birthday_kids <= 12
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
